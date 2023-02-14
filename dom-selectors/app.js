ec4f229e9a1238896952f7fcb6206670a158fa07
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-02-14 09:14:43 +0100

    update

---
+++ dom-selectors/app.js
@@ -0,0 +1,10 @@
+// document.getElementById()
+
+document.getElementById("task-title");
+
+// get thing from the element
+document.getElementById("task-title").id;
+document.getElementById("task-title").className;
+
+// change styling
+document.getElementById("task-title").style.background = "red";
