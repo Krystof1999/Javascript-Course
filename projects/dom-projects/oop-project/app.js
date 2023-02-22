08ac72a5795c218cab17e3d25e05d96b156ade86
DESKTOP-J4A1050\kryst <Krystofbumbalek2@gmail.com>
2023-02-22 21:53:02 +0100

    update

--- projects/dom-projects/oop-project/app.js
+++ projects/dom-projects/oop-project/app.js
@@ -23,6 +23,47 @@ UI.prototype.addBookToList = function (book) {
   list.appendChild(row);
 };
 
+// Delete books
+UI.prototype.deleteBooks = () => {
+  document.addEventListener("click", (e) => {
+    if (e.target.className === "delete") {
+      e.target.parentElement.parentElement.remove();
+      // Instatiate UI
+      const ui = new UI();
+      // Show alert
+      ui.showAlert("Book has been removed", "success");
+    }
+  });
+};
+
+// Clear fields
+UI.prototype.clearFields = () => {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
+
+// Error alert
+UI.prototype.showAlert = (message, className) => {
+  // Create div
+  const div = document.createElement("div");
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parent
+  const container = document.querySelector(".container");
+  const form = document.querySelector("#book-form");
+
+  // Insert alert
+  container.insertBefore(div, form); // what we want to insert | what we want the new element insert before
+
+  // Timeout after 3 sec
+  setTimeout(() => {
+    document.querySelector(".alert").remove();
+  }, 3000);
+};
+
 // Event Listeners
 document.getElementById("book-form").addEventListener("submit", (e) => {
   // Form values
@@ -36,8 +77,26 @@ document.getElementById("book-form").addEventListener("submit", (e) => {
   // Instatiate UI
   const ui = new UI();
 
-  // Add book to list
-  ui.addBookToList(book);
+  // Validate
+  if (title === "" || author === "" || isbn === "") {
+    // Error alert
+    ui.showAlert("Please fill in all fields", "error");
+  } else {
+    // Add book to list
+    ui.addBookToList(book);
+
+    // Show alert
+    ui.showAlert("You have added a book", "success");
+
+    // Clear fields
+    ui.clearFields();
+  }
 
   e.preventDefault();
 });
+
+// Delete books
+
+// Instatiate UI
+const ui = new UI();
+ui.deleteBooks();
